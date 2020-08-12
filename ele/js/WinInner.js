8a97bdc044558f4c1a64317f2a48fbae8744a1b1
liutaixiong <531457277@qq.com>
2020-08-12 16:51:19 +0800

    add window resize event.

--- ele/js/WinInner.js
+++ ele/js/WinInner.js
@@ -3,6 +3,8 @@
 		this.eleType = "util";
 		this._width;
 		this._height;
+		this._handlers = [];
+		var context = this;
 	
 		WinInner.prototype._init = function() {
 			if (typeof(window.innerWidth) != "undefined") {
@@ -16,6 +18,14 @@
 				this._height = document.getElementsByTagName('body')[0].clientHeight;
 			}
 		};
+		WinInner.prototype._onResizeResponse = function(){
+			context._init();
+			if(context._handlers.length > 0){
+				for(var i = 0; i < context._handlers.length; i ++){
+					context._handlers[i](context._width,context._height);
+				}
+			}
+		};
 		
 		WinInner.prototype.getWidth = function(){
 			return this._width;
@@ -25,6 +35,13 @@
 			return this._height;
 		};
 		
+		WinInner.prototype.addResizeHandler = function(onResizeHandler){
+			if(typeof(onResizeHandler) == "function"){
+				this._handlers.push(onResizeHandler);
+			}
+		};
+		
 		this._init();
+		window.addEventListener("resize",this._onResizeResponse,false);
 	};
 })();
