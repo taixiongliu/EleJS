8de6d9cb5df3377499f5a5e5b8638530c6c202b5
liutaixiong <531457277@qq.com>
2020-12-01 17:15:33 +0800

    update line to quadraticCurve

--- ele/js/AreaLine.js
+++ ele/js/AreaLine.js
@@ -4,12 +4,11 @@
 // 	,max:1000
 // 	,X:[{text:"AAA",fieldName:"a"},{text:"B",fieldName:"b"},{text:"C",fieldName:"c"},{text:"D",fieldName:"d"},{text:"E",fieldName:"e"}]
 // 	,Y:["0","200","400","600","800","1000"]
-// 	,nodes:[{id:1,name:"总线Ab123",color:"#ff6600"},{id:2,name:"分支线",color:"#41BABD"}]
-// 	,values:[{lineId:1,value:{a:{key:"", value:30},b:{key:"90", value:90},c:{key:"50", value:50},d:{key:"60", value:60},e:{key:"80", value:80}}},
-// {lineId:2,value:{a:{key:"", value:200},b:{key:"600", value:600},c:{key:"300", value:300},d:{key:"400", value:400},e:{key:"600", value:600}}}]
+// 	,nodes:[{id:1,name:"分支线",color:"#41BABD", areaColor:"rgba(94,204,204, 0.6)"},{id:2,name:"总线Ab123",color:"#b502a4",areaColor:"rgba(211,0,201, 0.6)"}]
+// 	,values:[{lineId:1,value:{a:{key:"", value:200},b:{key:"600", value:600},c:{key:"300", value:300},d:{key:"400", value:400},e:{key:"600", value:600}}},{lineId:2,value:{a:{key:"", value:30},b:{key:"90", value:90},c:{key:"50", value:50},d:{key:"60", value:60},e:{key:"80", value:80}}}]
 // 	};
 // areaLine.draw(data);
-(function() {
+// (function() {
 	var AreaLine = Ele.AreaLine = function(opts) {
 		this.eleType = "canvas";
 		this.ele;
@@ -28,6 +27,7 @@
 		this.itemColor = "#41BABD"; //节点颜色a0e0a3
 		this.itemlineWidth = 1; //节点线条宽度
 		this.itemPointWeight = 4; //节点半径
+		this.offset = 0.4; //偏移量 0-1之间 落差百分比
 		this.data;
 		this.filter = new Ele.Filter();
 		
@@ -83,6 +83,9 @@
 			if (typeof(opts.itemPointWeight) == "number") {
 				this.itemPointWeight = opts.itemPointWeight;
 			}
+			if (typeof(opts.offset) == "number") {
+				this.offset = opts.offset;
+			}
 		};
 		AreaLine.prototype._create = function() {
 			this._init();
@@ -99,7 +102,7 @@
 		AreaLine.prototype.setContainerById = function(id) {
 			document.getElementById(id).appendChild(this.ele);
 		};
-		//画折线图
+		//画区域图
 		AreaLine.prototype.draw = function(data) {
 			if(typeof(data) != "object"){
 				return ;
@@ -171,13 +174,17 @@
 					}
 				}
 				if(node == null || typeof(node.color) != "string"){
-					this.drawArrayData(vheight, nodeHeight, vNodeX, nodeWidth, hNodeY, arr, this.itemColor);
+					this.drawArrayData(vheight, nodeHeight, vNodeX, nodeWidth, hNodeY, arr, this.itemColor, this.edgeLineColor);
 				}else{
-					this.drawArrayData(vheight, nodeHeight, vNodeX, nodeWidth, hNodeY, arr, node.color);
+					var areaColor = this.edgeLineHintColor;
+					if(typeof(node.areaColor) == "string"){
+						areaColor = node.areaColor;
+					}
+					this.drawArrayData(vheight, nodeHeight, vNodeX, nodeWidth, hNodeY, arr, node.color, areaColor);
 				}
 			}
 		};
-		AreaLine.prototype.drawArrayData = function(vheight, nodeHeight, vNodeX, nodeWidth, hNodeY, data, color) {
+		AreaLine.prototype.drawArrayData = function(vheight, nodeHeight, vNodeX, nodeWidth, hNodeY, data, color, areaColor) {
 			if(typeof(this.data.max) != "number"){
 				return ;
 			}
@@ -213,7 +220,7 @@
 			
 				points.push({x:x,y:y})
 			}
-			this.drawQuadraticCurvePx(points, vNodeX, hNodeY);
+			this.drawQuadraticCurvePx(points, vNodeX, hNodeY, areaColor);
 		};
 		
 		AreaLine.prototype.drawEdgeLine = function(nodeHeight,vNodeX, nodeWidth, hNodeY, top) {
@@ -302,37 +309,65 @@
 			this.ctx.moveTo(sx, sy);
 			this.ctx.lineTo(ex, ey);
 		};
-		AreaLine.prototype.drawQuadraticCurvePx = function(points, vNodeX, hNodeY) {
+		AreaLine.prototype.drawQuadraticCurvePx = function(points, vNodeX, hNodeY, areaColor) {
 			if(points.length < 2){
 				return ;
 			}
 			
-			var cx;
-			var cy;
-			this.ctx.fillStyle=this.edgeLineHintColor;
+			this.ctx.fillStyle = areaColor;
 			this.ctx.beginPath();
 			this.ctx.moveTo(vNodeX, hNodeY);
 			this.ctx.lineTo(points[0].x,points[0].y);
+			var px = points[0].x;
+			var py = points[0].y;
+			//横坐标均等，两次找中点被划分为四段
+			var offsetX = (points[1].x - points[0].x)/4 * this.offset;
 			for(var i = 1; i < points.length; i ++){
-				this.ctx.lineTo(points[i].x,points[i].y);
-				// var tempX = (points[i].x + points[i-1].x)/2;
-				// var tempY = (points[i].y + points[i-1].y)/2;
-
-				// if(i == 2){
-				// 	this.ctx.moveTo(points[i-2].x, points[i-2].y);
-				// 	this.ctx.quadraticCurveTo(points[i-1].x -20, points[i-1].y-20, points[i-1].x, points[i-1].y);
-					
-				// 	this.ctx.stroke();
-				// 	cx = tempX;
-				// 	cy = tempY;
-				// 	continue;
-				// }
+				//两点间的中点
+				var centerX = (points[i].x + points[i-1].x)/2;
+				var centerY = (points[i].y + points[i-1].y)/2;
+				var type = (points[i].y - points[i-1].y) > 0?2:1;//1=增 2=减
+				
+				//中点距前一点的中点
+				var tempX = (centerX + points[i-1].x)/2;
+				var tempY = (centerY + points[i-1].y)/2;
+				console.log(tempX+"-"+tempY);
+				var offsetY = 0;
+				if(type == 1){
+					offsetY = (points[i-1].y - tempY) * this.offset;
+					tempX = tempX + offsetX;
+					tempY = tempY + offsetY;
+				}else{
+					offsetY = (tempY - points[i-1].y) * this.offset;
+					tempX = tempX + offsetX;
+					tempY = tempY - offsetY;
+				}
+				console.log(offsetX+"-"+offsetY);
+				//中点前半段
+				this.ctx.quadraticCurveTo(tempX, tempY, centerX, centerY);
+				
+				//中点距后一点的中点
+				tempX = (centerX + points[i].x)/2;
+				tempY = (centerY + points[i].y)/2;
+				offsetY = 0;
+				if(type == 1){
+					offsetY = (tempY - points[i].y) * this.offset;
+					tempX = tempX - offsetX;
+					tempY = tempY - offsetY;
+				}else{
+					offsetY = (points[i].y - tempY) * this.offset;
+					tempX = tempX - offsetX;
+					tempY = tempY + offsetY;
+				}
+				//中点后半段
+				this.ctx.quadraticCurveTo(tempX, tempY, points[i].x, points[i].y);
+				this.ctx.stroke();
 			}
 			this.ctx.lineTo(points[points.length - 1].x, hNodeY);
 			//this.ctx.lineTo(0, 0);
 			
 			// //
-			//this.ctx.stroke();
+			// this.ctx.stroke();
 			this.ctx.fill();
 			this.ctx.closePath();
 		};
